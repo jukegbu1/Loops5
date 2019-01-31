c51a383c2110485e7518cb0c60002294bc7e4cbd
jukegbu1 <jukegbu1@gmail.com>
2019-01-31 14:55:03 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int sum =0;
+        for(int i=1; i<=10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum " + sum);
+
+    }
+}
